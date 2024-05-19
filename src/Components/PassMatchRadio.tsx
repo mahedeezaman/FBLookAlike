927498ca820266fff6ce78a655b4f6c2bcb4e0f7
Kobiraj <mmoon@laurentian.ca>
2024-05-19 03:15:22 -0400

    Fixing shrink issue

--- src/Components/PassMatchRadio.tsx
+++ src/Components/PassMatchRadio.tsx
@@ -9,7 +9,7 @@ export default function PassMatchPropsRadio({ conditionMet, description }: PassM
 	return (
 		<>
 			<div className="flex items-center py-[10px]">
-				<div className={`mr-[10px] w-[20px] h-[20px] flex items-center justify-center rounded-full ${conditionMet ? 'bg-[#0A3C55]' : 'bg-[#B5C4CC]'}`}>
+				<div className={`mr-[10px] w-[20px] h-[20px] flex flex-shrink-0 items-center justify-center rounded-full ${conditionMet ? 'bg-[#0A3C55]' : 'bg-[#B5C4CC]'}`}>
 					{conditionMet && <FaCheck className="text-white w-[15px] h-[15px]" />}
 				</div>
 				<label className="mr-[15px]">{description}</label>
